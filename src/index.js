94f7fac55e4f1eab2c23db69b8ad5e561e40cdc5
ChaganYangoz <caganyangoz@gmail.com>
2023-09-22 13:31:50 +0300

    add todo

--- src/index.js
+++ src/index.js
@@ -1,54 +1,69 @@
-const container=document.querySelector("#container");
-const leftbar=document.createElement("div");
-leftbar.setAttribute("id","leftbar");
+import Todaytodo from "./today";
 
-const todoBar=document.createElement("div");
-todoBar.setAttribute('id','todoBar');
+const container = document.querySelector("#container");
+const leftbar = document.createElement("div");
+leftbar.setAttribute("id", "leftbar");
+
+const todoBar = document.createElement("div");
+todoBar.setAttribute('id', 'todoBar');
 
 container.appendChild(leftbar);
 container.appendChild(todoBar);
 
-const home=document.createElement("div");
-home.textContent="Home";
+const home = document.createElement("div");
+home.textContent = "Home";
 
-const today=document.createElement("div");
-today.textContent="Today";
+const today = document.createElement("div");
+today.textContent = "Today";
 
-const Week=document.createElement("div");
-Week.textContent="Week";
+const Week = document.createElement("div");
+Week.textContent = "Week";
 
-const addTodo=document.createElement("div");
-addTodo.textContent="+";
+const addTodo = document.createElement("div");
+addTodo.textContent = "+";
+
+const submit=document.querySelector("#submit");
+submit.onclick=sendForm;
+
+function sendForm(){
+  let title=document.querySelector("#title").value;
+  let desc=document.querySelector("#desc").value;
+}
 
 leftbar.appendChild(home);
 leftbar.appendChild(today);
 leftbar.appendChild(Week);
 leftbar.appendChild(addTodo);
 
-const todos=[];
+const todos = [];
 
-addTodo.setAttribute('id',"myBtn");
-addTodo.onclick=add;
+addTodo.setAttribute('id', "myBtn");
+addTodo.onclick = add;
 
-function add(){
-    todos.push('a');
-    console.log(todos);
+function add() {
+  todos.push('a');
+  console.log(todos);
+  Todaytodo();
+  modal.style.display = "block";
+
 }
 
 //Modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+
+class MergeToDo{
+  constructor(title,desc,date,pri){
+    this.title=title;
+    this.desc=desc;
+    this.date=date;
+    this.pri=pri;
   }
 }

6fd627cf5675185e6f69a9a45209ca6679a5c97e
ChaganYangoz <caganyangoz@gmail.com>
2023-09-24 15:42:47 +0300

    can send form

--- src/index.js
+++ src/index.js
@@ -22,12 +22,28 @@ Week.textContent = "Week";
 const addTodo = document.createElement("div");
 addTodo.textContent = "+";
 
-const submit=document.querySelector("#submit");
-submit.onclick=sendForm;
+const submit = document.querySelector("#submit");
+submit.onclick = sendForm;
 
-function sendForm(){
-  let title=document.querySelector("#title").value;
-  let desc=document.querySelector("#desc").value;
+function sendForm() {
+
+  let title = document.querySelector("#title").value;
+  let desc = document.querySelector("#desc").value;
+  let date = document.querySelector("#date").value;
+  var ele = document.getElementsByName('radio');
+
+  for (let i = 0; i < ele.length; i++) {
+    if (ele[i].checked){
+      let mrg=new MergeToDo(title,desc,date,ele[i].value);
+    ele[i].checked=false;}
+  }
+
+  document.querySelector("#title").value = "";
+  document.querySelector("#desc").value = "";
+  document.querySelector("#date").value = "";
+
+  modal.style.display = "none";
+
 }
 
 leftbar.appendChild(home);
@@ -41,9 +57,6 @@ addTodo.setAttribute('id', "myBtn");
 addTodo.onclick = add;
 
 function add() {
-  todos.push('a');
-  console.log(todos);
-  Todaytodo();
   modal.style.display = "block";
 
 }
@@ -59,11 +72,11 @@ window.onclick = function (event) {
 }
 
 
-class MergeToDo{
-  constructor(title,desc,date,pri){
-    this.title=title;
-    this.desc=desc;
-    this.date=date;
-    this.pri=pri;
+class MergeToDo {
+  constructor(title, desc, date, pri) {
+    this.title = title;
+    this.desc = desc;
+    this.date = date;
+    this.pri = pri;
   }
 }
